e11dcc6599f513202c416fa20163dede81282b28
lisahalman <61053821+lisahalman@users.noreply.github.com>
2021-12-01 10:11:37 +0100

    Ready for day 2

---
+++ src/Day2.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class Day2 {
+
+    public static void main(String[] args) throws IOException {
+        //first part
+        List<String> lines = Files.readAllLines(new File("Day1Input.txt").toPath());
+    }
+}
