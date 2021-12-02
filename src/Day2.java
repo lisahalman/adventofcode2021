e7dcc63754804a6da7c60ece41a618dd34d747c1
lisahalman <61053821+lisahalman@users.noreply.github.com>
2021-12-02 09:51:00 +0100

    Ready for day 2

--- src/Day2.java
+++ src/Day2.java
@@ -7,6 +7,33 @@ public class Day2 {
 
     public static void main(String[] args) throws IOException {
         //first part
-        List<String> lines = Files.readAllLines(new File("Day1Input.txt").toPath());
+        List<String> lines = Files.readAllLines(new File("Day2Input.txt").toPath());
+        int forward = 0;
+        int depth = 0;
+        for (String line : lines) {
+            if (line.charAt(0) == 'f') {
+                forward += Integer.parseInt(line.substring(line.length() - 1));
+            } else if (line.charAt(0) == 'd') {
+                depth += Integer.parseInt(line.substring(line.length() - 1));
+            } else if (line.charAt(0) == 'u') {
+                depth -= Integer.parseInt(line.substring(line.length() - 1));
+            }
+        }
+        System.out.println(forward * depth);
+        //second part
+        int newForward = 0;
+        int newDepth = 0;
+        int aim = 0;
+        for (String line : lines) {
+            if (line.charAt(0) == 'f') {
+                newForward += Integer.parseInt(line.substring(line.length() - 1));
+                newDepth += (Integer.parseInt(line.substring(line.length() - 1)) * aim);
+            } else if (line.charAt(0) == 'd') {
+                aim += Integer.parseInt(line.substring(line.length() - 1));
+            } else if (line.charAt(0) == 'u') {
+                aim -= Integer.parseInt(line.substring(line.length() - 1));
+            }
+        }
+        System.out.println(newDepth * newForward);
     }
 }
